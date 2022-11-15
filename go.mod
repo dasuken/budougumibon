0da01e9b33252c87709c80a6d83f590371ce93c8
github dasuken <daisukenoguemon@gmail.com>
2022-11-15 17:18:11 +0900

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dasuken/budougumibon
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
+		}),
+	}
+
+	return s.ListenAndServe()
+}
