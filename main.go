2c2bbb8fc08bc8e9ff9336ad0248729265fc371d
github dasuken <daisukenoguemon@gmail.com>
2022-11-16 12:15:49 +0900

    add listen

--- main.go
+++ main.go
@@ -4,20 +4,31 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Print("need port number \n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", p))
+	if err != nil {
+		log.Fatalf("failed to listen port %s, err: %v \n", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
@@ -26,7 +37,7 @@ func run(ctx context.Context) error {
 	// 引数のcontext使わなかったら、親のcancelが届かない
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
