045001b79a6a65629365aa86487a1311d4df05a8
github dasuken <daisukenoguemon@gmail.com>
2022-12-12 08:39:09 +0900

    impl: sqlx interface

--- store/repository.go
+++ store/repository.go
@@ -30,3 +30,34 @@ func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	xdb := sqlx.NewDb(db, "postgres")
 	return xdb, func() { _ = db.Close() }, nil
 }
+
+type Beginner interface {
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
+
+type Preparer interface {
+	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	Preparer
+	QueryxContext(ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error)
+	QueryRowxContext(ctx context.Context, query string, args ...interface{}) *sqlx.Row
+	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
+}
+
+var (
+	// インターフェイスが期待通りに宣言されているか確認
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.Tx)(nil)
+)
+
