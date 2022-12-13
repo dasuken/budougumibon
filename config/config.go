c1e1feec133ddc93fc72b216109873863b975f6e
dasuken <daisukenoguemon@gmail.com>
2022-12-13 21:18:29 +0900

    d

--- config/config.go
+++ config/config.go
@@ -8,13 +8,13 @@ import (
 
 // add port and TODO_ENV for port
 type Config struct {
-	Env  string `env:"TODO_ENV" envDefault:"dev"`
-	Port int    `env:"PORT" envDefault:"18080"`
-	DBHost string `env:"TODO_DB_HOST" envDefault:"127.0.0.1"`
-	DBPort string `env:"TODO_DB_PORT" envDefault:"5432"`
-	DBUser string `env:"TODO_DB_USER" envDefault:"todo"`
+	Env        string `env:"TODO_ENV" envDefault:"dev"`
+	Port       int    `env:"PORT" envDefault:"18080"`
+	DBHost     string `env:"TODO_DB_HOST" envDefault:"127.0.0.1"`
+	DBPort     int    `env:"TODO_DB_PORT" envDefault:"5432"`
+	DBUser     string `env:"TODO_DB_USER" envDefault:"todo"`
 	DBPassword string `env:"TODO_DB_PASSWORD" envDefault:"todo"`
-	DBName string `env:"TODO_DB_NAME" envDefault:"todo"`
+	DBName     string `env:"TODO_DB_NAME" envDefault:"todo"`
 }
 
 func New() (*Config, error) {
